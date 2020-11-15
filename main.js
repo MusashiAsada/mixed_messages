811de0f975c59bfc951da6f59036acc6d2b384c6
MusashiAsada <72708656+MusashiAsada@users.noreply.github.com>
2020-11-15 16:55:21 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+
+const black = [1, 2, 3, 4, 5, 6];
+const white = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+
+//Genererar random mening
+const randomSentence = {
+    //Genererar ett svart kort
+generateRandomBlack (arrayBlack) {
+    const randomNum = Math.floor(Math.random()*(arrayBlack.length));
+    const blackWord = arrayBlack[randomNum];
+    return blackWord;
+},
+//Genererar ett random vitt kort
+ generateRandomWhite (arrayWhite) {
+    const randomNum = Math.floor(Math.random()*(arrayWhite.length));
+    const whiteWord = arrayWhite[randomNum];
+    return whiteWord;
+},
+generateRandomSentence (){
+    const sentence = this.generateRandomBlack(black) + ' ' + this.generateRandomWhite(white);
+    return console.log(sentence);
+}
+}
+
+randomSentence.generateRandomSentence();
